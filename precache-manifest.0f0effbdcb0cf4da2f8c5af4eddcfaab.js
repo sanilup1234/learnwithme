40154383ec5e2684ebbe85538a96ffb21a3c6443
sanilup1234 <sanil.upadhyay12@gmail.com>
2020-05-24 01:35:20 +0530

    Updates

---
+++ precache-manifest.0f0effbdcb0cf4da2f8c5af4eddcfaab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bdcce7c62b8f59dc1b0fef7726cfce",
+    "url": "/learnwithme/index.html"
+  },
+  {
+    "revision": "5e22c3c8d4a598159445",
+    "url": "/learnwithme/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "9357286a622019fb4365",
+    "url": "/learnwithme/static/css/main.21d967e1.chunk.css"
+  },
+  {
+    "revision": "5e22c3c8d4a598159445",
+    "url": "/learnwithme/static/js/2.f598cd4e.chunk.js"
+  },
+  {
+    "revision": "f99e05d53c32fb31e343e8c538b8ef9c",
+    "url": "/learnwithme/static/js/2.f598cd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9357286a622019fb4365",
+    "url": "/learnwithme/static/js/main.8df96755.chunk.js"
+  },
+  {
+    "revision": "ed528f7c72e1cd32e55f",
+    "url": "/learnwithme/static/js/runtime-main.3a54fecd.js"
+  }
+]);
